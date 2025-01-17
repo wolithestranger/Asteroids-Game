916310135c306c8358ba11d41d5e23314f36d852
Daniel Nimbe-Oviosu <danieloviosu@gmail.com>
2025-01-16 22:54:47 -0500

    boilerplate game loop added

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+
+from constants import * #import everything from constants file... * means everything
+#from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        #fill(color, rect=None, special_flags=0) -> rect
+        screen.fill(color = (0,0,0))#000000
+        pygame.display.flip() #refreshes the screen
+
+if __name__ == "__main__":
+    main()
